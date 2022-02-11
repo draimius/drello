15b82e879361b80a8d97f3da14884b3df925ecc4
draimius <jrsworkshop17@gmail.com>
2022-02-11 07:30:20 -0600

    basic css and example html and js note

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 'use strict';
+//so what needs to happen inregards to the first elemetns and bing able to add more then
+//-be able to scoll horizontally with multiple list of item that also cn fill till bettom of screen
+//--and be able to scoll the list up and down when have lost of listed items
+//be able to add and delete each list item and each list
+//adding color choosing and card ect.. not really big deal maybe just some colors like list, item, background
+//hjow can i identify which list in target
+//have to be able to create delete while list and list items
+//be able to edit list title , list item
+//and be able to move the list items to diff order
+// we know the action we want to be able to do. now how do we achieve those?????/
+
+class Kanban {
+  constructor() {}
+  //edit text for target title or list item
+  //move list item within its own list
+  //move list item to another list
+  //add new list item
+  //add new list
+  //delete list
+  //delete list item
+  //and i think that pretty much it for all the functionalities really
+}
+//i think for the floats we'll maybe do that in another file
+
+//
+//
+//
+//what const will i need to create here what dom object do i need
+//also we arent gonna start with a list on screen so need a add list button on screen
