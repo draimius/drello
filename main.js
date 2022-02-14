c6dbe42a6aa715b2e5d6eb36e743413dca97dac7
draimius <jrsworkshop17@gmail.com>
2022-02-13 22:31:08 -0600

    working on popUp forms on/off for insertion

--- main.js
+++ main.js
@@ -30,87 +30,270 @@ const trello = new Kanban();
 const frame = document.querySelector('#frame');
 const addListForm = document.querySelector('#add-list-form');
 const addListButton = document.querySelector('#add-list-button');
+//use in card creating/inserting and related events
+let newCardForm = document.querySelector('#card-input');
+let addCardForms = document.querySelectorAll('.add-card-form');
+let addCardButtons = document.querySelectorAll('.add-card-button');
+let cardContianers = document.querySelectorAll('.item-container');
+//used in list creating/inserting and related events
+let newListButton = document.querySelector('#list-submit');
+let newListForm = document.querySelector('#list-input');
+let listXOut = document.querySelector('#x');
 
-addListButton.addEventListener('click', (e) => {
-  if (addListForm.children.length === 1) {
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+//=======hold thing is that only one popUp form should exist at a time never multiple
+//adding item to existing list
+function addCardListener() {
+  addCardButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      button.classList.add('adding-card');
+      let addCardForm = button.parentElement;
+      let targetList = addCardForm.parentElement.children[1];
+      // console.log(addCardForm); //the form includes button and text input
+      // console.log(targetList); //the ul container list where all item stored
+      if (addCardForm.children.length === 1) {
+        const popUpCardInput = document.createElement('form');
+        popUpCardInput.innerHTML = `
+        <input autocomplete="off" placeholder="enter list title" type="text" id="card-input" />
+        <div class="popUp">
+        </div>`;
+        newCardForm = document.querySelector('#card-input');
+        addCardForm.appendChild(popUpCardInput);
+        updateDynamicVars();
+        cardClickOut(addCardForm, button);
+        //after this
+        cardInputKey(targetList, addCardForm);
+        buttonClickOut(addCardForm, button);
+        newCardForm.focus();
+      }
+    });
+  });
+}
+
+function cardInputKey(targetList, addCardForm) {
+  newCardForm = document.querySelector('#card-input');
+  newCardForm.addEventListener('keypress', (e) => {
+    console.log('text input keypress event runnint');
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (newCardForm.value !== '') {
+        // addListForm.removeChild(form);
+        // addNewCard(newCardForm.value);
+        newCardForm.value = '';
+      }
+    }
+  });
+}
+// function addNewCard(cardText, targetList) {
+//   const card = document.createElement('li');
+//   card.draggable = true;
+//   card.classList.add('list-item');
+//   card.classList.add('draggable');
+//   card.textContent = cardText;
+//   targetList.appendChild(card);
+// }
+function buttonClickOut(addCardForm, button) {
+  newCardForm = document.querySelector('#card-input');
+
+  addCardButtons.forEach((btn) => {
+    if (btn.classList.length === 1) {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault;
+        e.stopPropagation();
+        if (addCardForm.children[1]) {
+          addCardForm.children[1].remove();
+        }
+        button.classList.remove('adding-card');
+      });
+    }
+  });
+}
+
+function cardClickOut(addCardForm, button) {
+  newCardForm = document.querySelector('#card-input');
+
+  document.querySelector('body').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(e);
-    const form = document.createElement('from');
-    form.innerHTML = `
-    <input autocomplete="off" placeholder="enter list title" type="text" id="list-input" />
-    <div class="popUp">
-    <button type="button" id="list-submit">add list</button>
-    <button type="button" id="x">X</button>
-    </div>`;
-    addListForm.appendChild(form);
-    listInputKey(form);
-    listButton(form);
-    listX(form);
-    // e.preventDefault();
-  }
-  draggables = document.querySelectorAll('.draggable');
-  dragCointainers = document.querySelectorAll('.item-container');
-});
+    e.stopPropagation();
 
+    // console.log(otherButtons);
+    if (e.target !== button && e.target !== newCardForm) {
+      if (addCardForm.children.length > 1 && addCardForm.children[1]) {
+        addCardForm.children[1].remove();
+      }
+      button.classList.remove('adding-card');
+      console.log('CLICK OFF DELETING');
+    }
+  });
+}
+
+///****when i open the form its one /// when i close the form its off */
+// let newCardForm = document.querySelector('#card-input');
+// let addCardForms = document.querySelectorAll('.add-card-form');
+// let addCardButtons = document.querySelectorAll('.add-card-button');
+//-have to check all other if target = another button but the one am one we close form
+// let cardContianers = document.querySelectorAll('.item-container');
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+
+//creates list and appends it
 function addNewList(title) {
   let newList = document.createElement('div');
-  newList.classList.add('list');
-  // newList.classList.add('draggable');
-  // newList.draggable = true;
-
+  newList.classList.add('list'); // add <span>...</span> later for deleting list
   newList.innerHTML = `
   <h3 class="list-title">${title}</h3>
   <ul class="item-container">
-  <li draggable="true" class="list-item draggable">1</li>
-  <li draggable="true" class="list-item draggable">2</li>
-  <li draggable="true" class="list-item draggable">3</li>
   </ul>
   <form class="add-card-form">
     <button type="button" class="add-card-button">add +</button>
   </form>
   `;
   frame.insertBefore(newList, addListForm);
-  draggables = document.querySelectorAll('.draggable');
-  dragCointainers = document.querySelectorAll('.item-container');
-  // console.log(draggables, dragCointainers);
-  // [...dragCointainers].push(newList);
-  updateDrags();
-  updateDragsContainers();
+  updateDynamicVars();
+  newListForm.focus();
+  addListForm.scrollIntoView();
 }
 
-function listInputKey(form) {
+//add list button form popUp
+addListButton.addEventListener('click', (e) => {
+  if (addListForm.children.length === 1) {
+    console.log('main click popUp event triggered');
+    e.preventDefault();
+    e.stopPropagation();
+
+    let popUpListInput = document.createElement('form');
+    popUpListInput.innerHTML = `
+    <input autocomplete="off" placeholder="enter list title" type="text" id="list-input" />
+    <div class="popUp">
+    <button type="button" id="list-submit">add list</button>
+    <button type="button" id="x">X</button>
+    </div>`;
+    addListForm.appendChild(popUpListInput);
+    updateDynamicVars();
+    newListForm.focus();
+
+    //everything after this still evaluating
+    listInputKey(popUpListInput);
+    listButton(popUpListInput);
+    listXButton(popUpListInput);
+    listClickOut(popUpListInput); //originally here (testting else where) work fine for actual x function
+  }
+});
+
+//all three below are listener for specified event and prevent others
+//=====================================================================================
+//submition button and the enter key both work as inteded maintain focus and create
+function listInputKey(popUpListInput) {
   let newListForm = document.querySelector('#list-input');
-  newListForm.focus();
   newListForm.addEventListener('keypress', (e) => {
-    console.log(e.keyCode);
+    console.log('text input keypress event runnint');
     if (e.keyCode === 13) {
       e.preventDefault();
+      e.stopPropagation();
+
       if (newListForm.value !== '') {
-        addListForm.removeChild(form);
+        // addListForm.removeChild(form);
         addNewList(newListForm.value);
+        newListForm.value = '';
       }
     }
   });
 }
-
-function listButton(form) {
+function listButton(popUpListInput) {
   let newListButton = document.querySelector('#list-submit');
   let newListForm = document.querySelector('#list-input');
   newListButton.addEventListener('click', (e) => {
     e.preventDefault();
+    e.stopPropagation();
+
+    console.log('submit button click event runnint');
     if (newListForm.value !== '') {
-      addListForm.removeChild(form);
+      // addListForm.removeChild(form);
       addNewList(newListForm.value);
+      newListForm.value = '';
     }
   });
 }
+//button click exit works fine
+//=====================================================================================
 
-function listX(form) {
+function listXButton(popUpListInput) {
   const listXOut = document.querySelector('#x');
   listXOut.addEventListener('click', (e) => {
     e.preventDefault();
-    addListForm.removeChild(form);
+    e.stopPropagation();
+
+    if (popUpListInput !== null) {
+      addListForm.removeChild(popUpListInput);
+      popUpListInput = null;
+      console.log('X BUTTON DELETING');
+    }
   });
+}
+//=====================================================================================
+
+function listClickOut(popUpListInput) {
+  const listXOut = document.querySelector('#x');
+
+  document.querySelector('body').addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    //either one of the buttons or the popUp text input
+    if (
+      e.target !== addListForm &&
+      e.target !== addListButton &&
+      e.target !== newListForm &&
+      e.target !== newListButton &&
+      e.target !== listXOut
+    ) {
+      if (addListForm.children.length > 1 && addListForm.children[1]) {
+        addListForm.children[1].remove();
+        // window.document.removeEventListener('click', listClickOut, true);
+      }
+      // addListForm.children[1].remove;
+      // test = test === undefined ? test
+      popUpListInput = null;
+    }
+  });
+  //if the popUp form exist and the e.target !== addlistForm they we remove the popUp and turn of listener
+}
+//its value for the
+// if has to come after the dynamic form is created
+
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+//=====================================================================================
+
+//update create all dynamic vars needed
+function updateDynamicVars() {
+  addCardButtons = document.querySelectorAll('.add-card-button');
+  addCardForms = document.querySelectorAll('.add-card-form');
+  cardContianers = document.querySelectorAll('.item-container');
+  updateDrags();
+  updateDragsContainers();
+  newListButton = document.querySelector('#list-submit');
+  newListForm = document.querySelector('#list-input');
+  listXOut = document.querySelector('#x');
+  addCardListener();
+  newCardForm = document.querySelector('#card-input');
+  draggables = document.querySelectorAll('.draggable');
+  dragCointainers = document.querySelectorAll('.item-container');
 }
 
 //=====================================================================================
@@ -129,20 +312,25 @@ function listX(form) {
 //capture all draggable items and containers
 let draggables = document.querySelectorAll('.draggable');
 let dragCointainers = document.querySelectorAll('.item-container');
-updateDrags();
-updateDragsContainers();
+// updateDrags();
+// updateDragsContainers();
+//all below needed to run once the elements were created we running before they were originally
 function updateDrags() {
   // function dragEvents(draggables, dragCointainers) {
   //=============================================
-  console.log(dragCointainers);
-  console.log(draggables);
+  // console.log(dragCointainers);
+  // console.log(draggables);
   //listen for drag on item
   draggables.forEach((item) => {
-    item.addEventListener('dragstart', () => {
+    item.addEventListener('dragstart', (e) => {
+      e.stopPropagation();
+
       item.classList.add('dragging');
     });
     //listent for drop on item
-    item.addEventListener('dragend', () => {
+    item.addEventListener('dragend', (e) => {
+      e.stopPropagation();
+
       item.classList.remove('dragging');
     });
   });
@@ -152,6 +340,8 @@ function updateDragsContainers() {
   dragCointainers.forEach((container) => {
     container.addEventListener('dragover', (e) => {
       e.preventDefault();
+      e.stopPropagation();
+
       //target element we'll be drop after(top of)
       const afterElement = getDragAfterElement(container, e.clientY);
       //if do target can grab things other then item and create issues
